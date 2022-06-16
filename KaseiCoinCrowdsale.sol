7fb62d23ec638d486ec3a83ce859e6632f4716d5
pete petersen <petepetersen423@gmail.com>
2022-06-16 00:26:14 -0700

    First Compile

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -4,21 +4,22 @@ import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
-
 // Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
-contract KaseiCoinCrowdsale is KaseiCoinCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
-    
+contract KaseiCoinCrowdsale is
+    KaseiCoinCrowdsale // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
+{
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
-    constructor(
+    constructor()
+        public
         // YOUR CODE HERE!
-    ) public Crowdsale(rate, wallet, token) {
+        Crowdsale(rate, wallet, token)
+    {
         // constructor can stay empty
     }
 }
 
-/*
 contract KaseiCoinCrowdsaleDeployer {
     // Create an `address public` variable called `kasei_token_address`.
     // YOUR CODE HERE!
@@ -26,26 +27,19 @@ contract KaseiCoinCrowdsaleDeployer {
     // YOUR CODE HERE!
 
     // Add the constructor.
-    constructor(
-       // YOUR CODE HERE!
-    ) public {
+    constructor() public // YOUR CODE HERE!
+    {
         // Create a new instance of the KaseiCoin contract.
         // YOUR CODE HERE!
-        
         // Assign the token contract’s address to the `kasei_token_address` variable.
         // YOUR CODE HERE!
-
         // Create a new instance of the `KaseiCoinCrowdsale` contract
         // YOUR CODE HERE!
-            
         // Aassign the `KaseiCoinCrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
         // YOUR CODE HERE!
-
         // Set the `KaseiCoinCrowdsale` contract as a minter
         // YOUR CODE HERE!
-        
         // Have the `KaseiCoinCrowdsaleDeployer` renounce its minter role.
         // YOUR CODE HERE!
     }
 }
-*/
