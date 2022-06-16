3ae7884f64562af98f2a6463743a554219a4e247
pete petersen <petepetersen423@gmail.com>
2022-06-16 00:20:56 -0700

    starter

--- KaseiCoin.sol
+++ KaseiCoin.sol
@@ -9,3 +9,5 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 // Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+
+constructor 
